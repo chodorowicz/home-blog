852af329d7e5a6de36394760cc1f78895e4c694d
Jakub Chodorowicz <chodorowicz@gmail.com>
2023-04-11 21:02:08 +0200

    add iframe component
    Signed-off-by: Jakub Chodorowicz <chodorowicz@gmail.com>

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,9 +1,23 @@
 import Image from 'next/image'
 import CustomLink from './Link'
+// import { SandboxCanvas } from '../data/blog/test.jsx'
+
+function IFrame({ src, style, title }) {
+  return (
+    <iframe
+      src={src}
+      style={style}
+      title={title}
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    />
+  )
+}
 
 const MDXComponents = {
   Image,
   a: CustomLink,
+  IFrame,
 }
 
 export default MDXComponents
