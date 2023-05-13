f9652ebe37b320846a05d91baaa7052f4d8f1334
Jakub Chodorowicz <chodorowicz@gmail.com>
2023-05-13 12:50:18 +0200

    Fix canonical rendering
    Signed-off-by: Jakub Chodorowicz <chodorowicz@gmail.com>

--- components/SEO.js
+++ components/SEO.js
@@ -42,7 +42,7 @@ export const PageSeo = ({ title, description, url }) => {
         title,
         description,
       }}
-      {...(!!canonical && { canonical })}
+      {...(!!url && { canonical: url })}
     />
   )
 }
